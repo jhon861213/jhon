d9c5b7cf59fc5858dad0120619a492a89c07c0f9
jhon861213 <133544492+jhon861213@users.noreply.github.com>
2023-05-31 22:15:16 -0400

    Add files via upload

---
+++ EXAMEN FINAL/examen final.js
@@ -0,0 +1,37 @@
+
+let correo = prompt("ingrese correo");
+const num1 = NaN;
+
+    
+
+if (correo != num1 && correo != (" ")) {
+ 
+  
+} else if (correo == endwith("cumbre.edu.bo")) {
+
+    
+} else {
+    const randNum = Math.random();
+    const num = Math.floor(Math.random()*11);
+    const now = new Date();
+          now.getDate(); 
+    let mensaje ="correo no valido vuelva a intentarlo" 
+    alert(mensaje.concat(`${Math.floor(Math.random()*11)} ${now.getDate()}`));
+
+}
+
+let pasword = prompt("ingrese contraseña");         
+
+if (pasword > 8) {
+    
+} else if (pasword.startsWith($)) {
+    const consulta = confirm("desea agregar un post");
+} else {
+
+    const randNum = Math.random();
+    const num = Math.floor(Math.random()*11);
+    const now = new Date();
+          now.getDate(); 
+    let mensaje ="correo no valido vuelva a intentarlo" 
+    alert(mensaje.concat(`${Math.floor(Math.random()*11)} ${now.getDate()}`));
+}
